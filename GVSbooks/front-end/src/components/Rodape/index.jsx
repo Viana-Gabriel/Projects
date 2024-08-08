08b86e4355de33df096d1fb689b66a8cdeb89f57
Bowie <gabriel.viana.gv02@gmail.com>
2024-08-08 17:40:02 -0300

    GVSbooks - Finalizado
    Ajustes Finais

--- GVSbooks/front-end/src/components/Rodape/index.jsx
+++ GVSbooks/front-end/src/components/Rodape/index.jsx
@@ -3,7 +3,9 @@ import style from './Rodape.module.css'
 export default function Rodape() {
   return (
     <footer className={`${style.Rodape} flex`}>
-
+      <h2>
+      &copy; Desenvolvido por Gabriel Viana - 2024
+      </h2>
     </footer>
   )
 }
